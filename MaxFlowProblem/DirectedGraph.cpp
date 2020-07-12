8645bcf6996a745261e3518632be5eac6d50c1ba
Ishak Yotam <yotamishak@gmail.com>
2020-07-12 07:50:23 -0700

    DAG

--- MaxFlowProblem/DirectedGraph.cpp
+++ MaxFlowProblem/DirectedGraph.cpp
@@ -1,1 +1,56 @@
 ﻿#include "DirectedGraph.h"
+#include <cstring>
+
+DirectedGraph::DirectedGraph(int n)
+{
+	MakeEmptyGraph(n);
+}
+
+DirectedGraph::~DirectedGraph()
+{
+	for (int i = 0; i < _size; i++)
+	{
+		delete[]_AdjacencyMatrix[i];
+	}
+	delete[] _AdjacencyMatrix;
+	
+}
+// note: why need?????????????????????????
+void DirectedGraph::MakeEmptyGraph(int n)
+{
+
+	_size = n;
+	_AdjacencyMatrix = new int*[n];
+	for(int i=0; i< _size ; i++)
+	{
+		_AdjacencyMatrix[i] = new int[n];
+
+		for(int j=0; j< _size; j++)
+		{
+			_AdjacencyMatrix[i][j] = 0;
+		}
+		//memset(_AdjacencyMatrix[i], 0, sizeof(int) * sizeof _size);
+	}
+}
+
+bool DirectedGraph::IsAdjacent(int u, int v) const
+{
+	return _AdjacencyMatrix[u][v];
+}
+//note: what to return?????????????????????????
+int* DirectedGraph::GetAdjList(int u) const
+{
+	int* AdjList = new int[_size];
+	memcpy(AdjList, _AdjacencyMatrix[u], sizeof(int));
+	return AdjList;
+}
+
+void DirectedGraph::AddEdge(int u, int v, int capacity) const
+{
+	_AdjacencyMatrix[u][v] = capacity;
+}
+
+void DirectedGraph::RemoveEdge(int u, int v) const
+{
+	_AdjacencyMatrix[u][v] = 0;
+}
--- MaxFlowProblem/DirectedGraph.h
+++ MaxFlowProblem/DirectedGraph.h
@@ -2,6 +2,16 @@
 
 class DirectedGraph
 {
-public:
+private:
+	int** _AdjacencyMatrix;
+	int _size;
 	
+public:
+	DirectedGraph(int n);
+	~DirectedGraph();
+	void MakeEmptyGraph(int n);
+	bool IsAdjacent(int u, int v) const;
+	int* GetAdjList(int u) const;
+	void AddEdge(int u, int v, int capacity) const;
+	void RemoveEdge(int u, int v) const;
 };
