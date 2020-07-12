f58998991904ee13011bbb6908a38014c1ceb119
Ishak Yotam <yotamishak@gmail.com>
2020-07-12 07:14:39 -0700

    queue

--- MaxFlowProblem/Queue.cpp
+++ MaxFlowProblem/Queue.cpp
@@ -1,1 +1,54 @@
 ﻿#include "Queue.h"
+
+#include <iostream>
+using namespace std;
+Queue::Queue()
+{
+	head = new Node;
+	tail = head;
+}
+
+Queue::~Queue()
+{
+	MakeEmpty();
+}
+
+void Queue::MakeEmpty()
+{
+	while(!IsEmpty())
+	{
+		Node* temp = head;
+		head = head->next;
+		delete temp;
+	}
+	
+}
+
+int Queue::IsEmpty()
+{
+	return head == tail;
+}
+
+void Queue::EnQueue(int item)
+{
+	Node *newNode = new Node(item);
+	tail->InsertAfter(newNode);
+	tail = newNode;
+}
+
+int Queue::DeQueue()
+{
+	if (IsEmpty())
+	{
+		cout << "Error: QUEUE EMPTY!!" << endl;
+		exit(1);
+	}
+	Node *temp = head->DeleteAfter();
+	int item = temp->data;
+	if (tail == temp)
+	{
+		tail = head;
+	}
+	delete temp;
+	return item;
+}
--- MaxFlowProblem/Queue.h
+++ MaxFlowProblem/Queue.h
@@ -1,7 +1,16 @@
 ﻿#pragma once
-
+#include "Node.h"
 class Queue
 {
+private:
+	Node *head, *tail;
+
 public:
-	
+	Queue();
+	~Queue();
+	void MakeEmpty();
+	int IsEmpty();
+	int Front();
+	void EnQueue(int item);
+	int DeQueue();
 };
